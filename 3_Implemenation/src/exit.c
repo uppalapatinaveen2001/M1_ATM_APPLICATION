2f0eebab96524ed961b2acdb81134977045a611e
uppalapatinaveen2001 <55971705+uppalapatinaveen2001@users.noreply.github.com>
2022-03-31 10:13:18 +0530

    Add files via upload

---
+++ 3_Implemenation/src/exit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include"atm.h"
+
+int menuExit() {
+    printf("--------------Take your receipt!!!------------------\n");
+    printf("-----Thank you for using ATM Banking Machine!!!-----\n");
+    return 5;
+
+}
