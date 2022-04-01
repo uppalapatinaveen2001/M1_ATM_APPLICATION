b2bca1eb618b3be43621c9fa1c83c59f79fe4f46
uppalapatinaveen2001 <55971705+uppalapatinaveen2001@users.noreply.github.com>
2022-04-01 14:13:21 +0530

    Update deposit.c

--- 3_Implemenation/src/deposit.c
+++ 3_Implemenation/src/deposit.c
@@ -7,7 +7,7 @@
 float moneyDeposit(float balance) {
     float deposit;
     printf("You choose to Deposit a money\n");
-    printf("$$$$Your Balance is: $%.2f\n\n", balance);
+    printf("your money is there in account$%.2f\n\n", balance);
     printf("Enter your amount to Deposit\n");
     scanf("%f", &deposit);
 
