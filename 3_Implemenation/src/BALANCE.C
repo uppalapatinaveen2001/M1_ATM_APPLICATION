f94bbf0d911b8d88eaa84cf7eee894939e30e5e8
uppalapatinaveen2001 <55971705+uppalapatinaveen2001@users.noreply.github.com>
2022-04-01 15:52:15 +0530

    Update BALANCE.C

--- 3_Implemenation/src/BALANCE.C
+++ 3_Implemenation/src/BALANCE.C
@@ -4,7 +4,7 @@
 #include <math.h>
 #include"atm.h"
 
-float checkBalance(float balance) {
+float checkBala(float balance) {
     printf("your balance is......\n");
     printf("\n\n++++Your Available Balance is:   $%.2f\n\n", balance);
     return balance;
